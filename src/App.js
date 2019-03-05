024804edd90d22d2dbb7a06c94d89f9b3f71ff39
Allie <alicemdeegan@gmail.com>
2019-03-05 12:52:45 -0500

    API call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Qs from 'qs';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+    axios({
+      url: 'https://proxy.hackeryou.com',
+      dataResponse: 'json',
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: `https://www.goodreads.com/search/`,
+        params: {
+          q: 'Twilight',
+          key: 'WpabDZgBfnIW2CiYFtXKw',
+          search: 'all'
+        },
+        proxyHeaders: {
+          'header_params': 'value',
+        },
+        xmlToJSON: true
+      }
+    }).then(response => {
+      console.log(response);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
