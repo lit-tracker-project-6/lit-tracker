a3eb666b8741773e4abc018117a1b2530cadcc5c
Derek <derekluu90@gmail.com>
2019-03-07 14:56:07 -0500

    error handling for Search.js API call

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,34 +13,40 @@ class Search extends Component {
   }
 
   
-  // getSearchResults = (event) => {
-  //   event.preventDefault();
-  //   axios({
-  //     url: 'https://proxy.hackeryou.com',
-  //     dataResponse: 'json',
-  //     paramsSerializer: function (params) {
-  //       return Qs.stringify(params, { arrayFormat: 'brackets' })
-  //     },
-  //     params: {
-  //       reqUrl: `https://www.goodreads.com/search/`,
-  //       params: {
-  //         q: this.state.searchQuery,
-  //         key: 'WpabDZgBfnIW2CiYFtXKw',
-  //         search: 'all'
-  //       },
-  //       proxyHeaders: {
-  //         'header_params': 'value',
-  //       },
-  //       xmlToJSON: true
-  //     }
-  //   }).then(response => {
-  //     const res = response.data.GoodreadsResponse.search.results.work
-  //     console.log(res);
-  //     this.setState({
-  //       searchResults: res
-  //     })
-  //   })
-  // }
+  getSearchResults = (event) => {
+    event.preventDefault();
+    axios({
+      url: 'https://proxy.hackeryou.com',
+      dataResponse: 'json',
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: `https://www.goodreads.com/search/index.xml`,
+        params: {
+          q: this.state.searchQuery,
+          key: 'WpabDZgBfnIW2CiYFtXKw',
+          search: 'all'
+        },
+        proxyHeaders: {
+          'header_params': 'value',
+        },
+        xmlToJSON: true
+      }
+    }).then(response => {
+      const res = response.data.GoodreadsResponse.search.results.work
+      console.log(res);
+      this.setState({
+        searchResults: res
+      })
+    })
+  }
+
+  noResults = () => {
+    return (
+      <p>No results, please try another search.</p>
+    )
+  }
 
   handleChange = (event) => {
     this.setState({
@@ -48,9 +54,32 @@ class Search extends Component {
     })
   }
 
+  printSearchedBooks = () => {
+
+    const searchedBooks = this.state.searchResults.map(data => {
+      return (
+
+        !isNaN(data.average_rating) &&
+        //when there is no average_rating, the value is an object. Return the below JSX when the value is a number (negative NaN)
+
+        <div key={data.id["$t"]} className="bookOption" data-key={data.id["$t"]}>
+          <img src={data.best_book.image_url} alt={`Book cover of ${data.best_book.title}`} />
+          <p>{data.best_book.title}</p>
+          <p>{data.best_book.author.name}</p>
+          <p>{data.average_rating}</p>
+        </div>
+        // console.log(data.id)
+
+      )
+    })
+
+    return searchedBooks
+  }
+
 
   render() {
     return (
+
       <div className="search">
         <h2>This is the search modal</h2>
         <p onClick={this.props.handleSearchModalOff}>close this modal</p>
@@ -63,16 +92,11 @@ class Search extends Component {
 
         <div className="searchedBooks">
           {
-            this.state.searchResults.map(data => {
-              return(
-                <div key={data.id["$t"]} className="bookOption" data-key={data.id["$t"]}>
-                  <img src={data.best_book.image_url} alt={`Book cover of ${data.best_book.title}`} />
-                  <p>{data.best_book.title}</p>
-                  <p>{data.best_book.author.name}</p>
-                  <p>{data.average_rating}</p>
-                </div>
-              )
-            })
+            //if searchResults is truthy (when there are no results, searchResults is undefined) print the results otherwise print the error handling message
+            this.state.searchResults ?
+            //don't use length or it will always call noResults - because searchResults is initialized as an empty array
+            this.printSearchedBooks() :
+            this.noResults()
           }
 
         </div>
